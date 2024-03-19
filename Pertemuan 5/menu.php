bf30bd204236500019d8be4f9c075d1981835d40
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-03-19 12:40:28 +0700

    Pertemuan 5 Contoh Kasus Menu Bertingkat

---
+++ Pertemuan 5/menu.php
@@ -0,0 +1,53 @@
+<?php
+$menu =[
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama"=> "Kontak"
+    ],
+];
+
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item['nama']}";
+
+        // utk periksa submenu item
+        if (isset($item['subMenu'])) {
+            // memanggil jika ada
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+
+        echo "</li>";
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
+?>
